63d50b2a5db83794af9e5487cb41da40e0e85980
Prem270920 <sathishpremkumar27@gmail.com>
2026-01-29 00:50:10 +0530

    Modified and Fixed ingestion script

--- src/ingestion/ingest_bitcoin.py
+++ src/ingestion/ingest_bitcoin.py
@@ -1,14 +1,15 @@
 import requests
 import pandas as pd
 from datetime import datetime
+import os
+
+# This points to crypto-stream/data/raw/bitcoin_data_lake
+output_dir = os.path.join("data", "raw", "bitcoin_data_lake")
+os.makedirs(output_dir, exist_ok=True)
 
 # Define the API endpoint and parameters
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
-params = {
-    'vs_currency': 'usd',
-    'days': '30',
-    'interval': 'daily'
-}
+params = {'vs_currency': 'usd', 'days': '30', 'interval': 'daily'}
 
 print("Fetching data from CoinGecko...")
 response = requests.get(url, params=params)
@@ -23,3 +24,8 @@ df["date"] = df["timestamp"].dt.strftime('%Y-%m-%d')
 
 print(f"Data fetched! Rows: {len(df)}")
 print(df.head())
+
+# Save to the new structured path
+print(f"Saving to {output_dir}...")
+df.to_parquet(output_dir, partition_cols=['date'])
+print("Data saved successfully.")
